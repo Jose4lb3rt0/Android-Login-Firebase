40956b07eddeb3a340cb678752cd9949129fe6bf
Jose4lb3rt0 <josealberto3200@gmail.com>
2025-02-26 00:59:44 -0500

    Navegación arreglada

--- app/src/main/java/pe/jadse/android_login_firebase/ui/Login.java
+++ app/src/main/java/pe/jadse/android_login_firebase/ui/Login.java
@@ -12,6 +12,7 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
 
 import com.google.firebase.auth.FirebaseAuth;
 
@@ -31,16 +32,20 @@ public class Login extends Fragment {
     @Override
     public void onDestroyView() {
         super.onDestroyView();
+        binding = null;
     }
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return super.onCreateView(inflater, container, savedInstanceState);
+        binding = FragmentLoginBinding.inflate(inflater, container,false );
+        return view = binding.getRoot();
     }
 
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+        context = getContext();
+        navController = Navigation.findNavController(view);
     }
 }
